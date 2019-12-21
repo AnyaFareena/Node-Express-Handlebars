94fe7140afb102d255b421383d7d18e371bfba12
Anya <anyaanto1909@gmail.com>
2019-12-20 22:23:32 -0500

    updated schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,4 @@
--- insert burgers here
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Pepper Jack Mushroom Burger', true);
