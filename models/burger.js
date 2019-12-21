11974c5cb472e1cae916219c8e4feaa01b45bbd4
Anya <anyaanto1909@gmail.com>
2019-12-20 22:45:04 -0500

    update models

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,28 @@
-// your model should do CRUD operations
+// Import the ORM to inserte functions that will interact with the database.
+var orm = require("../config/orm");
+
+var burger = {
+  selectAll: function (cb) {
+    orm.selectAll("burgers", function (res) {
+
+      cb(res);
+    });
+  },
+
+  insertOne: function (name, cb) {
+    orm.insertOne("burgers", name, cb)
+
+  },
+
+  updateOne: function (id, cb) {
+    orm.updateOne("burgers", id, cb);
+
+  },
+
+  deleteOne: function (id, cb, name) {
+    orm.deleteOne("burgers", id, cb, name, function (res) {
+      cb(res)
+    });
+  }
+};
+module.exports = burger;
