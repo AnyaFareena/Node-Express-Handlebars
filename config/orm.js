fbc9ced7c6922f8784842567c1da96c95b568d12
Anya <anyaanto1909@gmail.com>
2019-12-20 22:41:59 -0500

    update config

--- config/orm.js
+++ config/orm.js
@@ -1,3 +1,44 @@
+var connection = require("./connection");
 
-// Here is the O.R.M. where you write functions that takes inputs and conditions
-// and turns them into database commands like SQL.
+var orm = {
+  selectAll: function (tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+
+  insertOne: function (tableInput, val, cb) {
+    connection.query('INSERT INTO ' + tableInput + "(burger_name) VALUES ('" + val + "');",
+      function (err, result) {
+        if (err) {
+          throw err;
+        }
+        cb(result);
+      });
+  },
+
+  updateOne: function (tableInput, condition, cb) {
+    connection.query('UPDATE ' + tableInput + ' SET devoured=true WHERE id=' + condition + ';', function (err, result) {
+      if (err) throw err;
+      cb(result);
+    })
+  },
+
+  deleteOne: function (tableInput, condition, cb) {
+    var queryString = "DELETE FROM " + tableInput;
+    queryString += " WHERE id=";
+    queryString += condition;
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+};
+
+module.exports = orm;
