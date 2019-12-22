4b52d48f843bce90068a55e88ff74c6e4ba8ffb5
Anya <anyaanto1909@gmail.com>
2019-12-21 22:02:21 -0500

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,18 @@ var mysql = require("mysql");
 
 var connection;
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+	// DB is JawsDB on Heroku
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "root@1234",
-    database: "burger_db"
-  });
-}
+	// DB is local on localhost
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'root@1234',
+		database: 'burgers_db'
+	})
+};
 
 connection.connect(function(err) {
   if (err) {
